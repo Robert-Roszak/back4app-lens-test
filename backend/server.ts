c1da13ce2e27777fc16f237e003471ec7933b6c4
Robert-Roszak <79662439+Robert-Roszak@users.noreply.github.com>
2023-06-08 20:48:16 +0200

    add dburl debuging

--- backend/server.ts
+++ backend/server.ts
@@ -39,11 +39,12 @@ app.use('*', (req, res) => {
 
 /* MONGOOSE */
 const NODE_ENV = process.env.NODE_ENV;
+console.log('RRTEST NODE_ENV: ' + NODE_ENV);
 let dbUri = '';
 if (NODE_ENV === 'production') dbUri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.gtv2z.mongodb.net/lensShop?retryWrites=true&w=majority`;
 else if (NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/lensShoptest';
 else dbUri = 'mongodb://localhost:27017/lensShop';
-
+console.log('RRTEST dbUri: ' + dbUri);
 mongoose.set('strictQuery', true);
 
 mongoose.connect(dbUri);
